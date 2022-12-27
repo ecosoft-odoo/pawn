e99d258374130a2e24f24e7e8051daad6e996c7b
newtratip <tharathip.chaweewongphan@gmail.com>
2022-12-27 13:35:27 +0700

    [ADD] pawn_migration

---
+++ pawn_migration/__openerp__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pawnshop - Migration Script",
+    "version": "7.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Pawnshop Management",
+    "author": "Ecosoft",
+    "depends": [
+        "pawnshop",
+    ],
+    "data": [
+        "views/pawn_views.xml",
+    ],
+}
---
+++ pawn_migration/models/pawn.py
@@ -0,0 +1,34 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.osv import fields, osv
+
+
+class pawn_order(osv.osv):
+    _inherit = "pawn.order"
+
+    _columns = {
+        "book": fields.integer(readonly=False),
+        "number": fields.integer(readonly=False),
+    }
+
+    def _get_next_pawn_name(self, cr, uid, period_id, pawn_shop_id, context=None):
+        """next name, number and book in data migration no need auto"""
+        return "", 0, 0
+
+    def create(self, cr, uid, vals, context=None):
+        book, number = vals.get("book", 0), vals.get("number", 0)
+        pawn_id = super(pawn_order, self).create(cr, uid, vals, context=context)
+        self.write(cr, uid, [pawn_id], {
+            "book": book,
+            "number": number,
+        })
+        return pawn_id
+
+    def write(self, cr, uid, ids, vals, context=None):
+        res = super(pawn_order, self).write(cr, uid, ids, vals, context=context)
+        for pawn in self.browse(cr, uid, ids, context=context):
+            name = pawn.pawn_shop_id.code + pawn.period_id.fiscalyear_id.code + str(pawn.book).zfill(3) + str(pawn.number).zfill(3)
+            if name != pawn.name:
+                self.write(cr, uid, [pawn.id], {"name": name})
+        return res
