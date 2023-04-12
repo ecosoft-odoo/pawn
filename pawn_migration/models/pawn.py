93e13df2a9ce96bad2bf03fd773eda14b216ab21
newtratip <tharathip.chaweewongphan@gmail.com>
2023-04-12 20:00:13 +0700

    [FIX] pawn_migration

--- pawn_migration/models/pawn.py
+++ pawn_migration/models/pawn.py
@@ -43,22 +43,13 @@ class pawn_order(osv.osv):
             PawnOrder.write(cr, uid, [pawn_order.id], {"name": pawn_order.name}, context=context)
         # Update data in pawn.order.line
         PawnOrderLine = self.pool.get("pawn.order.line")
-        line_ids = PawnOrderLine.search(cr, uid, [("order_id.state", "=", "draft")], context=context)
+        line_ids = PawnOrderLine.search(cr, uid, [("order_id", "in", pawn_order_ids)], context=context)
         lines = PawnOrderLine.browse(cr, uid, line_ids, context=context)
         for line in lines:
             price = PawnOrderLine._amount_line(cr, uid, [line.id], ["price_unit", "pawn_price_unit"], None)[line.id]
             cr.execute("""
                 update pawn_order_line set price_unit = %s, pawn_price_unit = %s where id = %s
             """ % (price["price_unit"] or 0.0, price["pawn_price_unit"] or 0.0, line.id))
-        # Update data in product.template
-        ProductTemplate = self.pool.get("product.template")
-        product_template_ids = ProductTemplate.search(cr, uid, [("state", "=", "draft")], context=context)
-        product_templates = ProductTemplate.browse(cr, uid, product_template_ids, context=context)
-        for product_template in product_templates:
-            if product_template.order_id:
-                cr.execute("""
-                    update product_template set journal_id = %s where id = %s
-                """ % (product_template.order_id.journal_id.id, product_template.id))
         # Update data in product.product
         ProductProduct = self.pool.get("product.product")
         product_ids = ProductProduct.search(cr, uid, [("state", "=", "draft")], context=context)
@@ -70,6 +61,15 @@ class pawn_order(osv.osv):
             cr.execute("""
                 update product_product set item_description = '%s', price_estimated = %s, price_pawned = %s, total_price_estimated = %s, total_price_pawned = %s, product_qty_total = %s where id = %s
             """ % (item_description, price["price_estimated"] or 0.0, price["price_pawned"] or 0.0, price["total_price_estimated"] or 0.0, price["total_price_pawned"] or 0.0, qty or 0.0, product.id))
+        # Update data in product.template
+        ProductTemplate = self.pool.get("product.template")
+        product_template_ids = ProductTemplate.search(cr, uid, [("id", "in", [p.product_tmpl_id.id for p in products])], context=context)
+        product_templates = ProductTemplate.browse(cr, uid, product_template_ids, context=context)
+        for product_template in product_templates:
+            if product_template.order_id:
+                cr.execute("""
+                    update product_template set journal_id = %s where id = %s
+                """ % (product_template.order_id.journal_id.id, product_template.id))
 
     def action_pawn(self, cr, uid, context=None):
         PawnOrder = self.pool.get("pawn.order")
