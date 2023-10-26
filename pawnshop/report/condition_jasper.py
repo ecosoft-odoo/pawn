3219bce21e12a40bd6d83aea19fd59fe75c26ccc
newtratip <tharathip.chaweewongphan@gmail.com>
2023-10-26 13:28:06 +0700

    [FIX] Can print pawn order form in state cancel

--- pawnshop/report/condition_jasper.py
+++ pawnshop/report/condition_jasper.py
@@ -43,31 +43,31 @@ import openerp.exceptions
 from openerp.service import http_server
 from openerp import SUPERUSER_ID
 import openerp
- 
+
 class report_xml(osv.osv):
-    
+
     _inherit = 'ir.actions.report.xml'
-        
-        
+
+
     def _check_pawn_order(self, cr, uid, ids, context=None):
         for report in self.browse(cr, uid, ids, context=context):
             if report.report_name == 'pawn.order.form':
                 if context.get('active_ids', False):
                     for obj in self.pool.get('pawn.order').browse(cr, uid, context.get('active_ids')):
-                        if (obj.state in ['draft', 'cancel']):
+                        if (obj.state in ['draft']):
                             raise openerp.exceptions.Warning("Can't print form in %s state" % (obj.state, ))
         return True
-    
+
     def _check_report_condition(self, cr, uid, ids, context=None):
         self._check_pawn_order(cr, uid, ids, context=context)
-         
+
     def _report_content(self, cr, uid, ids, name, arg, context=None):
         self._check_report_condition(cr, uid, ids, context=context)
         return super(report_xml, self)._report_content(cr, uid, ids, name, arg, context=context)
- 
+
     def _report_content_inv(self, cursor, user, id, name, value, arg, context=None):
         return super(report_xml, self)._report_content_inv(cursor, user, id, name, value, arg, context=context)
-        
+
     _columns = {
         'report_sxw_content': fields.function(_report_content, fnct_inv=_report_content_inv, type='binary', string='SXW Content',),
         'report_rml_content': fields.function(_report_content, fnct_inv=_report_content_inv, type='binary', string='RML Content'),
